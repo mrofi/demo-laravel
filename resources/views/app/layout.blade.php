9ad9f278cf243bad57a97192ccd2ddc0ae27b680
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-01-21 10:01:20 +0700

    update layout links

--- resources/views/app/layout.blade.php
+++ resources/views/app/layout.blade.php
@@ -12,7 +12,7 @@
                 <div class="container mx-auto px-6 py-4">
                     <div class="flex items-center justify-between">
                         <div>
-                            <a class="flex items-center text-gray-800 hover:text-indigo-600" href="#">
+                            <a class="flex items-center text-gray-800 hover:text-indigo-600" href="{{ route('homepage') }}">
                                 <svg class="h-6 w-6 sm:h-6 sm:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"/>
                                 </svg>
@@ -21,7 +21,7 @@
                             </a>
                         </div>
                         <div class="flex items-center -mx-2">
-                            <a class="flex items-center mx-2 text-gray-800 hover:text-indigo-600" href="#">
+                            <a class="flex items-center mx-2 text-gray-800 hover:text-indigo-600" href="{{ route('articles.index') }}">
                                 <svg class="h-5 w-5 sm:h-6 sm:w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                 </svg>
@@ -53,7 +53,7 @@
                         </div>
 
                         <div class="flex items-center -mx-2">
-                            <a class="flex items-center mx-2 text-gray-600 hover:text-indigo-600" href="#">
+                            <a class="flex items-center mx-2 text-gray-600 hover:text-indigo-600" href="{{ route('articles.index') }}">
                                 <svg class="h-5 w-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M19 11H5M19 11C20.1046 11 21 11.8954 21 13V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V13C3 11.8954 3.89543 11 5 11M19 11V9C19 7.89543 18.1046 7 17 7M5 11V9C5 7.89543 5.89543 7 7 7M7 7V5C7 3.89543 7.89543 3 9 3H15C16.1046 3 17 3.89543 17 5V7M7 7H17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                                 </svg>
