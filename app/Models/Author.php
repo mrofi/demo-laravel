4ef7800f7f58775baa855a5f936ef1f9025db85b
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-01-24 09:02:19 +0700

    update author model add relation to user

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Author extends Model
@@ -21,4 +22,14 @@ class Author extends Model
     protected $casts = [
         'social_media' => 'array',
     ];
+
+    /**
+     * Get the user that owns the Author
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
