f617287596f520b1bfdc934610f0daae153cfd17
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-01-24 09:52:38 +0700

    add category factory and seeder

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->word();
+        $slug = Str::slug($name);
+
+        return [
+            'name' => $name,
+            'slug' => $slug,
+            'visible' => $this->faker->boolean(),
+        ];
+    }
+}
