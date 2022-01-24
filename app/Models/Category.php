8eecddfbee40e97b0b158a87a9274c5852d3fedd
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-01-24 09:16:11 +0700

    update model Category add relation to articles

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
@@ -16,4 +17,14 @@ class Category extends Model
         'slug',
         'visible',
     ];
+
+    /**
+     * Get all of the articles for the Category
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function articles(): HasMany
+    {
+        return $this->hasMany(Article::class, 'article_id');
+    }
 }
