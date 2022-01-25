4365913633d3d1948a526aa86715465bcf1f564d
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-01-25 12:22:27 +0700

    add role routes

--- routes/cms.php
+++ routes/cms.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Cms\ArticleController;
+use App\Http\Controllers\Cms\RoleController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('cms')
@@ -10,6 +11,8 @@ Route::prefix('cms')
             return view('cms.dashboard');
         })->name('cms.dashboard');
 
-        Route::resource('article', ArticleController::class)->names('cms.articles');
+        Route::resource('article', ArticleController::class)->names('cms.articles')->middleware('can:cms.articles.index');
+        Route::resource('role', RoleController::class)->names('cms.roles')->middleware('can:cms.roles.index');
+        Route::post('role/{role}/assign', [RoleController::class, 'assignPermission'])->name('cms.roles.assign_permission')->middleware('can:cms.roles.create');
     });
 
