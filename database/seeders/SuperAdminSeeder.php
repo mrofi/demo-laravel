76c221ef2ad963e807314c4feb8076ef90739d6d
Mokhamad Rofiudin <mokh.rofiudin@gmail.com>
2022-01-25 12:18:01 +0700

    add creating role in super admin seeder

--- database/seeders/SuperAdminSeeder.php
+++ database/seeders/SuperAdminSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class SuperAdminSeeder extends Seeder
 {
@@ -14,11 +15,20 @@ class SuperAdminSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $user = User::firstOrNew([
             'name' => 'Super Admin',
-            'email' => 'root@root',
+            'email' => 'root@root'
+        ]);
+
+        $user->fill([
             'password' => bcrypt('root'),
             'email_verified_at' => date('Y-m-d'),
         ]);
+
+        $user->save();
+
+        $role = Role::findOrCreate('super-admin');
+
+        $user->assignRole($role);
     }
 }
